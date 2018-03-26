cb003f72d102dad19ec692307ba63681eb4c9cb4
Vitorgus <26413854+Vitorgus@users.noreply.github.com>
2018-03-26 18:28:39 -0300

    forgot to add the files...

---
+++ commands/other/talk.js
@@ -0,0 +1,26 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class sayCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'talk',
+            group: 'other',
+            memberName: 'talk',
+            description: 'Mcore says the message in the specified server channel',
+            examples: ['mcore talk Hi there!'],
+            args: [{
+                key: 'text',
+                prompt: 'What do you want M_CORE to say on the server?',
+                type: 'string'
+            }]
+        });
+    }
+
+    run(msg, { text }) {
+        if (msg.author.id !== process.env.MAD) return;
+        return this.client.guilds.get(process.env.MAD_CHAT)
+            .channels.get(process.env.MAD_CHANNEL)
+            .send(text);
+        //return msg.say(text);
+    }
+};
